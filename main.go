6663250ffeeac3f1a092b016cc4403d142c79028
Mateusz Zawisza <mateusz.zawisza@gmail.com>
2014-10-13 12:57:48 +0200

    moves snapshot to /snapshot and adds main

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/mateuszzawisza/elastic-snapshot/snapshot"
+)
+
+var address = flag.String("address", "localhost:9200", "elasticsearch address:port")
+var action = flag.String("action", "list", "(list|create)")
+var repo = flag.String("repo", "my_repo", "snapshot repo")
+
+func init() {
+	if len(*repo) == 0 {
+		log.Panicf("Repo not set")
+	}
+}
+
+func main() {
+	flag.Parse()
+	if *action == "list" {
+		snap_name := fmt.Sprintf("snapshot_%s", time.Now)
+		snapshot.CreateSnapshot(*address, *repo, snap_name)
+	}
+}
