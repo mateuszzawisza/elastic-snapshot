8a9cb2726ab6909fdd8495753a916b9bb6260e4d
Mateusz Zawisza <mateusz.zawisza@gmail.com>
2014-10-14 14:35:05 +0200

    run tests before build

--- script/build.sh
+++ script/build.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
+set -e
+
+echo "Running tests..."
+./script/test.sh
+echo "Done!"
+echo
+
+echo "Building..."
 gox -output "./bin/elastic-snapshot_{{.OS}}_{{.Arch}}" -os "linux darwin"
+echo "Done!"
